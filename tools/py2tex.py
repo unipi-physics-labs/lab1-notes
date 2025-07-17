81ebc4a271e89e9fa0fd369eb5c9c1454066f386
Luca Baldini <luca.baldini@pi.infn.it>
2025-07-17 07:12:33 +0200

    Full rewrite of py2tex

--- tools/py2tex.py
+++ tools/py2tex.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2016, Luca Baldini (luca.baldini@pi.infn.it).
+# Copyright (C) 2025, Luca Baldini (luca.baldini@pi.infn.it).
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU GengReral Public License as published by
@@ -19,82 +19,82 @@
 
 import glob
 import os
+import pathlib
 import subprocess
 
-STATNOTES_BITBUCKET_URL = 'https://bitbucket.org/lbaldini/statnotes/src/master/'
-STATNOTES_BIN = os.path.abspath(os.path.dirname(__file__))
-STATNOTES_ROOT = os.path.abspath(os.path.join(STATNOTES_BIN, os.pardir))
-STATNOTES_TEXCODE = os.path.join(STATNOTES_ROOT, 'latex', 'texcode')
-STATNOTES_SNIPPETS = os.path.join(STATNOTES_ROOT, 'snippets')
+from loguru import logger
+
+
+STATNOTES_GITHUB_URL = 'https://github.com/unipi-physics-labs/statnotes/tree/main'
+
+STATNOTES_ROOT = pathlib.Path(__file__).resolve().parent.parent
+STATNOTES_TEXCODE = STATNOTES_ROOT / 'texcode'
+STATNOTES_SNIPPETS = STATNOTES_ROOT / 'snippets'
 
 
 NO_OUTPUT_SCRIPTS = []
 
 
-def pygmentize(file_path, label = True):
-    """Run pygments on a python script and generate the corresponding
-    LaTeX output.
-
-    This is achieved through something along the lines of:
-    pygmentize -f latex -O full -l python -o factorial_1.tex factorial_1.py
+def pygmentize(snippet_path: str = STATNOTES_SNIPPETS, label: bool = True):
+    """Run pygments on a python script and generate the corresponding LaTeX output.
     """
-    if os.path.isdir(file_path):
-        print('Pygmentizing folder %s...' % file_path)
-        for scriptPath in glob.glob(os.path.join(file_path, '*.py')):
-            pygmentize(scriptPath)
+    # First thing first, if the path to the snippet(s) is not a pathlib.Path
+    # instance, we turn it into one.
+    if not isinstance(snippet_path, pathlib.Path):
+        snippet_path = pathlib.Path(snippet_path)
+    # If the target path does not exist, we raise an exception.
+    if not snippet_path.exists():
+        raise RuntimeError(f'Target path {snippet_path} does not exist')
+    # If the target path is a folder, then we pygmentize all the Python files within
+    # the folder calling the function recursively.
+    if snippet_path.is_dir():
+        logger.info(f'Pygmentizing folder {snippet_path}...')
+        for _path in snippet_path.iterdir():
+            if _path.is_file() and _path.suffix == '.py':
+                pygmentize(_path)
         return
-    if not file_path.endswith('.py'):
-        print('%s does not look like a python file, skipping...'% file_path)
-        return
-    print('Running pygments...')
-    cmd = 'pygmentize -f latex -O full -l python %s' % file_path
-    process = subprocess.Popen(cmd, stdout = subprocess.PIPE,
-                               stderr = subprocess.PIPE, shell = True)
-    # At this point we have the full pygmentized output available and we
-    # do have to extract the relevant part.
-    file_path = os.path.abspath(file_path)
-    file_name = os.path.basename(file_path)
-    output_file_path = os.path.join(STATNOTES_TEXCODE,
-                                  file_name.replace('.py', '.tex'))
-    print('Generating output file %s...' % output_file_path)
-    output_file = open(output_file_path, 'w')
-    doWrite = False
-    for line in process.stdout.readlines():
-        line = line.decode()
-        if line.startswith('\\begin{Verbatim}'):
-            if label:
-                text = os.path.basename(file_path).replace('_', '\_')
-                folder = os.path.dirname(file_path).replace(STATNOTES_ROOT, '')
-                folder = folder.strip('\/')
-                #title = os.path.join(folder, text)
-                title = text
-                url = os.path.join(STATNOTES_BITBUCKET_URL, folder, text)
-                text = '\\makebox{\\href{%s}{https://bitbucket.org/.../%s}}' %\
-                       (url, title)
-                line = line.replace('[', '[label=%s,' % text)
-            doWrite = True
-        elif line.startswith('\\end{Verbatim}'):
-            doWrite = False
-        if doWrite:
-            output_file.write(line)
-    if os.path.basename(file_path) not in NO_OUTPUT_SCRIPTS:
-        os.chdir(os.path.dirname(file_path))
-        cmd = 'python %s' % file_path
-        process = subprocess.Popen(cmd, stdout = subprocess.PIPE,
-                                   stderr = subprocess.PIPE, shell = True)
-        error = process.stderr.read()
-        if error:
-            print(error)
-        output = process.stdout.read().decode()
-        if len(output):
-            print('Adding command output...')
-            text = '\n[Output]\n%s' % output
-            output_file.write(text)
-        os.chdir(STATNOTES_ROOT)
-    output_file.write('\\end{Verbatim}')
-    output_file.close()
-    print('Done, closing output file.')
+    elif snippet_path.is_file() and snippet_path.suffix != '.py':
+        raise RuntimeError(f'{snippet_path} is not a python file')
 
+    # Now we are good to go with a single, good Python file!
+    file_path = snippet_path.resolve()
+    logger.info(f'Pygmentizing {file_path}...')
+    # Pygmentize the script.
+    cmd = f'pygmentize -f latex -O full -l python {file_path}'
+    kwargs = dict(stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    snippet_tex = subprocess.Popen(cmd, **kwargs).stdout.read().decode()
+    # Note at this point the LaTeX source is complete document with a long preamble,
+    # and all we really care is whatever lies in the Verbatim block.
+    snippet_tex = snippet_tex.split(r'\begin{Verbatim}[commandchars=\\\{\}]')[1]
+    snippet_tex = snippet_tex.split(r'\end{Verbatim}')[0]
+
+    # If necessary, run the script to capture the output and append it to the tex.
+    if file_path.name not in NO_OUTPUT_SCRIPTS:
+        cmd = f'python {file_path}'
+        # Note in this case we run the process in the parent folder of the target
+        # script, in case the latter has relative paths to data files.
+        process = subprocess.Popen(cmd, cwd=file_path.parent, **kwargs)
+        errors = process.stderr.read().decode()
+        if errors:
+            logger.error(errors)
+        snippet_output = process.stdout.read().decode()
+        if len(snippet_output):
+            logger.debug(f'Command output:\n{snippet_output}')
+            snippet_tex = f'{snippet_tex}\n[Output]\n{snippet_output}'
+
+    # Complete the information with the url to the snippet on github.
+    url = f'{STATNOTES_GITHUB_URL}{str(file_path).replace(str(STATNOTES_ROOT), "")}'
+    title = f'https://github.com/.../{file_path.name}'
+    label = r'\makebox{\href{%s}{%s}}' % (url, title)
+
+    # We are ready to write the output file.
+    full_text = r'\begin{Verbatim}[label=%s,commandchars=\\\{\}]' % label
+    full_text = f'{full_text}{snippet_tex}'
+    full_text = '%s\end{Verbatim}\n' % full_text
+    output_file_path = STATNOTES_TEXCODE / f'{file_path.stem}.tex'
+    with open(output_file_path, 'w') as output_file:
+        output_file.write(full_text)
+    logger.info('Done!')
 
 
 if __name__ == '__main__':
