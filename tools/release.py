a9de944aa59642962b34f6d0553c9289f7870009
Luca Baldini <luca.baldini@pi.infn.it>
2026-08-28 10:19:41 +0200

    Minor.

--- tools/release.py
+++ tools/release.py
@@ -30,17 +30,17 @@ from loguru import logger
 
 # Configure the logger.
 logger.remove()
-logger.add(sink=sys.stderr, colorize=True, format='>>> <level>{message}</level>')
+logger.add(sink=sys.stderr, colorize=True, format=">>> <level>{message}</level>")
 
 
 STATNOTES_ROOT = pathlib.Path(__file__).resolve().parent.parent
-VERSION_FILE_PATH = STATNOTES_ROOT / 'version.tex'
-README_FILE_PATH = STATNOTES_ROOT / 'README.md'
-GITHUB_RELEASE_URL = 'https://github.com/unipi-physics-labs/lab1-notes/releases'
+VERSION_FILE_PATH = STATNOTES_ROOT / "version.tex"
+README_FILE_PATH = STATNOTES_ROOT / "README.md"
+GITHUB_RELEASE_URL = "https://github.com/unipi-physics-labs/lab1-notes/releases"
 
-INCREMENT_MODES = ('major', 'minor', 'patch')
+INCREMENT_MODES = ("major", "minor", "patch")
 
-_ENCODING = 'utf-8'
+_ENCODING = "utf-8"
 
 
 def execute_shell_command(arguments):
@@ -53,47 +53,47 @@ def execute_shell_command(arguments):
 def copy_file(src, dest):
     """ Copy a file in another location.
     """
-    logger.info('Copying {src} to {dest}...')
+    logger.info("Copying {src} to {dest}...")
     shutil.copy(src, dest)
 
 
 def _read_version():
     """ Read the version straight from the appropriate file.
     """
-    logger.info(f'Reading version from {VERSION_FILE_PATH}...')
+    logger.info(f"Reading version from {VERSION_FILE_PATH}...")
     with open(VERSION_FILE_PATH, encoding=_ENCODING) as version_file:
-        version = version_file.readline().strip('\n')
-    logger.debug(f'Current version is {version}')
+        version = version_file.readline().strip("\n")
+    logger.debug(f"Current version is {version}")
     return version
 
 
 def _write_version(version: str):
     """ Write the version to the appropriate file.
     """
-    logger.info(f'Writing version {version} to {VERSION_FILE_PATH}...')
-    with open(VERSION_FILE_PATH, 'w', encoding=_ENCODING) as version_file:
-        version_file.write(f'{version}\n')
+    logger.info(f"Writing version {version} to {VERSION_FILE_PATH}...")
+    with open(VERSION_FILE_PATH, "w", encoding=_ENCODING) as version_file:
+        version_file.write(f"{version}\n")
 
 
 def increment_version_file(mode: str) -> str:
     """Update the version.tex file.
     """
-    logger.info(f'Bumping version file (mode = {mode})...')
+    logger.info(f"Bumping version file (mode = {mode})...")
     if mode not in INCREMENT_MODES:
-        raise RuntimeError(f'Invalid increment mode "{mode}"---valid modes are {INCREMENT_MODES}')
+        raise RuntimeError(f"Invalid increment mode "{mode}"---valid modes are {INCREMENT_MODES}")
     old_version = _read_version()
-    major, minor, patch = (int(item) for item in old_version.split('.'))
-    if mode == 'major':
+    major, minor, patch = (int(item) for item in old_version.split("."))
+    if mode == "major":
         major += 1
         minor = 0
         patch = 0
-    elif mode == 'minor':
+    elif mode == "minor":
         minor += 1
         patch = 0
-    elif mode == 'patch':
+    elif mode == "patch":
         patch += 1
-    new_version = f'{major}.{minor}.{patch}'
-    logger.info(f'Target version is {new_version}')
+    new_version = f"{major}.{minor}.{patch}"
+    logger.info(f"Target version is {new_version}")
     _write_version(new_version)
     return new_version
 
@@ -101,53 +101,53 @@ def increment_version_file(mode: str) -> str:
 def compile_latex():
     """ Recompile the damned thing.
     """
-    execute_shell_command(['make'])
+    execute_shell_command(["make"])
 
 
 def _asset_url(name: str, version: str) -> str:
     """ Return the URL for an asset.
     """
-    return f'{GITHUB_RELEASE_URL}/download/{version}/{name}-{version}.pdf'
+    return f"{GITHUB_RELEASE_URL}/download/{version}/{name}-{version}.pdf"
 
 def write_readme(version: str) -> None:
     """ Write the README file.
     """
-    readme_path = STATNOTES_ROOT / 'README.md'
-    logger.info(f'Writing README file to {readme_path}...')
-    with open(readme_path, 'w', encoding=_ENCODING) as readme_file:
-        readme_file.write('# lab1-notes\n\n')
-        readme_file.write('Note e materiale didattico a supporto del corso di ')
-        readme_file.write('*Laboratorio con elementi di computazione*.\n\n')
-        readme_file.write('L\'ultima versione del materiale è la')
-        readme_file.write(f' [v{version}]({GITHUB_RELEASE_URL}/tag/{version}).\n\n')
-        readme_file.write('Link diretti al file pdf:\n\n')
-        readme_file.write(f'- [Statistica ed analisi dati]({_asset_url("statnotes", version)})\n')
-        readme_file.write('La pagina delle [release]({GITHUB_RELEASE_URL}) ')
-        readme_file.write('contiene lo storico delle versioni passate rilevanti.\
+    readme_path = STATNOTES_ROOT / "README.md"
+    logger.info(f"Writing README file to {readme_path}...")
+    with open(readme_path, "w", encoding=_ENCODING) as readme_file:
+        readme_file.write("# lab1-notes\n\n")
+        readme_file.write("Note e materiale didattico a supporto del corso di ")
+        readme_file.write("*Laboratorio con elementi di computazione*.\n\n")
+        readme_file.write("L\'ultima versione del materiale è la")
+        readme_file.write(f" [v{version}]({GITHUB_RELEASE_URL}/tag/{version}).\n\n")
+        readme_file.write("Link diretti al file pdf:\n\n")
+        readme_file.write(f"- [Statistica ed analisi dati]({_asset_url("statnotes", version)})\n")
+        readme_file.write("La pagina delle [release]({GITHUB_RELEASE_URL}) ")
+        readme_file.write("contiene lo storico delle versioni passate rilevanti.\n")
 
 def release(mode: str) -> None:
     """ Tag the package and create a release.
     """
-    execute_shell_command(['git', 'pull'])
+    execute_shell_command(["git", "pull"])
     version = increment_version_file(mode)
 
     # compile_latex()
     # Do stuff, e.g., copy pdf files around for archival purposes...
     # If you create new stuff, do not forget to push it on the repository!
 
-    msg = f'Prepare for tag {version}.'
-    execute_shell_command(['git', 'commit', '-a', '-m', msg])
-    execute_shell_command(['git', 'push'])
-    msg = f'Tagging version {version}...'
-    execute_shell_command(['git', 'tag', '-a', version, '-m', msg])
-    execute_shell_command(['git', 'push', '--tags'])
-    execute_shell_command(['git', 'status'])
-    logger.info(f'Release {version} completed successfully.')
+    msg = f"Prepare for tag {version}."
+    execute_shell_command(["git", "commit", "-a", "-m", msg])
+    execute_shell_command(["git", "push"])
+    msg = f"Tagging version {version}..."
+    execute_shell_command(["git", "tag", "-a", version, "-m", msg])
+    execute_shell_command(["git", "push", "--tags"])
+    execute_shell_command(["git", "status"])
+    logger.info(f"Release {version} completed successfully.")
 
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('mode', type=str, choices=INCREMENT_MODES, help='Version increment mode')
+    parser.add_argument("mode", type=str, choices=INCREMENT_MODES, help="Version increment mode")
     args = parser.parse_args()
     release(args.mode)
